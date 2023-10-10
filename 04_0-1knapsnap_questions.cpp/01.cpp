f749f57cbdd9c903c95a164259a9f21722caca2a
parth tagalpallewar <55437315+ParthTagalpallewar@users.noreply.github.com>
2023-10-10 13:00:35 +0530

    Update 01.cpp

--- 04_0-1knapsnap_questions.cpp/01.cpp
+++ 04_0-1knapsnap_questions.cpp/01.cpp
@@ -1,1 +1,14 @@
 // question link: https://practice.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1
+
+//todo 
+Two Sum
+Roman to Integer
+Palindrome Number
+Maximum Subarray
+Remove Element
+Contains Duplicate
+Add Two Numbers
+Majority Element
+Remove Duplicates from Sorted Array
+Valid Anagram
+Group Anagrams
